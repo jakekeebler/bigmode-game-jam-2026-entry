558ee92396eec9e98d4034e3deaee1b83d03d12d
Jake Keebler <jakekeebler@gmail.com>
2026-01-29 15:32:55 -0500

    primitive character gravity and movement with test scene

---
+++ player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export_subgroup("Nodes")
+@export var gravity_component: GravityComponent
+@export var input_component: InputComponent
+@export var movement_component: MovementComponent
+
+func _physics_process(delta: float) -> void:
+	gravity_component.handle_gravity(self, delta)
+	movement_component.handle_horizontal_movement(self, input_component.input_horizontal)
+	
+	move_and_slide()
